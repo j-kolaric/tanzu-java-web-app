cf88d34466c6a84c78c7af6934f1f285b2862019
Julien Kolaric <jkolaric@jkolaric56Y96.vmware.com>
2023-03-27 13:06:35 +0200

    app change

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot + Tanzu at Thales Alenia Space!";
+		return "Greetings from Spring Boot + Tanzu!";
 	}
 
 }
