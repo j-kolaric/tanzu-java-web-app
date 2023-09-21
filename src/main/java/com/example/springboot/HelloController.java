503c4465b8225d931900e51e7cfa15671a793fd0
Julien Kolaric <jkolaric@jkolaric56Y96.vmware.com>
2023-09-21 09:48:13 +0200

    test

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot + Tanzu for Bala !";
+		return "Greetings from Spring Boot + Tanzu !";
 	}
 
 }
